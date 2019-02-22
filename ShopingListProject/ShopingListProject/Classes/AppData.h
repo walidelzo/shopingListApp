9ef5fb1dede79e1efeb3c3c3f169478b2bb5965c
Admin <admin@As-MacBook-Pro.local>
2019-02-22 14:47:02 +0200

    config fireBase to App

--- ShopingListProject/ShopingListProject/Classes/AppData.h
+++ ShopingListProject/ShopingListProject/Classes/AppData.h
@@ -1,11 +1,19 @@
 #import <Foundation/Foundation.h>
 #import "ShopingListsClass.h"
+@import Firebase;
 NS_ASSUME_NONNULL_BEGIN
 
 @interface AppData : NSObject
 @property(nonatomic,retain)UserClass* curUser;
 @property(nonatomic,retain)NSMutableArray<ShopingListsClass*> * curLST;
 @property(nonatomic,retain)NSMutableArray <ShopingListsClass*> * offlineLST;
+@property(nonatomic,retain)NSMutableArray<ShopingListsClass*> *onlineLST;
+@property(nonatomic)FIRDatabaseReference *rootNode;
+@property(nonatomic)FIRDatabaseReference *userNode;
+@property(nonatomic)FIRDatabaseReference *dataNode;
+
+
+
 
 +(id)SharedManager;
 @end
--- ShopingListProject/ShopingListProject/Classes/AppData.m
+++ ShopingListProject/ShopingListProject/Classes/AppData.m
@@ -7,6 +7,11 @@
     self = [super init];
     if (self) {
         _curLST=[NSMutableArray new];
+        _onlineLST=[NSMutableArray new];
+        [FIRApp configure];
+        _rootNode=[[FIRDatabase database]reference];
+        _userNode=[_rootNode child:@"Users"];
+        _dataNode=[_rootNode child:@"Data"];
     }
     return self;
 }
